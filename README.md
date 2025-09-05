493dc277596d4dfdf87a3573b481382fec598c66
Rohit Shelar <rohitpshelar@gmail.com>
2025-09-06 01:46:11 +0530

    init

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ AWS, Java,21, Spring Boot 3
     2. Set up free billing alerts to avoid surprise charges
     3. Common mistakes beginners make (and how to avoid them)
 <img width="870" height="512" alt="image" src="https://github.com/user-attachments/assets/1a67b769-316c-41c5-9807-bbd3223d7648" />
+   4. IAM :  For User Management
+   5. 
---
+++ src/main/java/com/example/aws_java/AwsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.aws_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJavaApplication.class, args);
+	}
+
+}
